63b793d5487097ec77070f416f5ea4b485fb81b5
samdotkap <100087983+samdotkap@users.noreply.github.com>
2022-11-29 19:24:31 +0200

    schemas and fake mongodb uplink

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import connectDatabase from "./config/mongodb.js"
+
+dotenv.config();
+connectDatabase();
+const app = express();
+app.use(cors());
+
+//load all products
+app.get("/api/products",(req,res) =>{
+    res.json(products)
+})
+
+//load single product
+app.get("/api/products/id", (req, res) =>{
+    const product = products.find((p)=> p._id === req.params.id)
+    res.json(product)
+})
+
+app.get('/', (req, res) =>{
+    res.send("API is running....")
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, console.log(`server running on ${PORT}`))
