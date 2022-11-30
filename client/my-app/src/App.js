721625b954bb7c974e30abf0c3381939a0c72d5d
samdotkap <100087983+samdotkap@users.noreply.github.com>
2022-11-30 10:50:40 +0200

    routes

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import {Router, Route} from 'react'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Route path='/' component={replace} />
+      <Route path='/products/:id' component={replace} />
+      <Route path='/login' component={replace} />
+      <Route path='/register' component={replace} />
+      <Route path='/profile' component={replace} />
+      <Route path='/cart/:id' component={replace} />
+      <Route path='/shipping' component={replace} />
+      <Route path='/payment' component={replace} />
+      <Route path='/placeorder' component={replace} />
+      <Route path='/order' component={replace} />
+      <Route path='/*' component={replace} />
+    </Router>
   )
 }
